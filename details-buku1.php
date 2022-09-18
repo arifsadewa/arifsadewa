f3dcbd97028f1471058be02c3a3bfeadd6de178a
arifsadewa <113844185+arifsadewa@users.noreply.github.com>
2022-09-18 21:39:51 +0700

    Add files via upload

---
+++ details-buku1.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Katalog Details - CV Idea Sejahtera</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo.jpg" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Arsha - v4.8.0
+  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top header-inner-pages">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">Idea Sejahtera</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.jpg" alt="" class="img-fluid"></a> -->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link   scrollto" href="#portfolio">Katalog</a></li>
+          <li class="dropdown"><a href="#"><span>Layanan</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Penerbitan Buku</a></li>
+              <li><a href="#">Setting Layout</a></li>
+              <li><a href="#">Desain Grafis</a></li>
+              <li><a href="#">Finishing dan Jilid Binding</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="https://wa.me/085727274416">+62 857-2727-4416</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Katalog Details</li>
+        </ol>
+        <h2>Katalog Details</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Portfolio Details Section ======= -->
+    <section id="portfolio-details" class="portfolio-details">
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4">
+            <div class="portfolio-details-slider swiper">
+              <div class="swiper-wrapper align-items-center">
+
+                <div class="swiper-slide">
+                  <img src="assets/img/portfolio/buku1.jpg" alt="" style="width: 400px;">
+                </div>
+
+                <!-- <div class="swiper-slide">
+                  <img src="assets/img/portfolio/portfolio-2.jpg" alt="">
+                </div>
+
+                <div class="swiper-slide">
+                  <img src="assets/img/portfolio/portfolio-3.jpg" alt="">
+                </div> -->
+
+              </div>
+              <div class="swiper-pagination"></div>
+            </div>
+          </div>
+
+          <div class="col-lg-8">
+            <div class="portfolio-info">
+              <h3>LEARN LIKE NEVER BEFORE; Cara Mudah Memahami Bahasa Inggris</h3>
+              <ul>
+                <li><strong>Penulis</strong>: Isyqie Firdausah, M.Hum</li>
+                <li><strong>Tahun Terbit</strong>: 2016</li>
+                <li><strong>ISBN</strong>: 978-602-6335-27-2</li>
+                <li><strong>Edisi</strong>: I</li>
+                <li><strong>Halaman</strong>: x + 204 hlm</li>
+                <li><strong>Bahan</strong>: Book Paper</li>
+              </ul>
+            </div>
+            <div class="portfolio-description">
+              <h2>Sinopsis</h2>
+              <p>
+                Ragam lisan dan tulisan merupakan dua ragam utama dalam bahasa Inggris. Ragam tulisan menuntut grammar yang fasih serta vocabulary yang secara terminologis tepat guna karena digunakan dalam situasi formal dan dunia tulis menulis. Banyak pemula mengeluh raham ini kaku dan membuat bahasa Inggris tampak rumit. Di sisi lain, ragam lisan “hanya” menuntut speaking skill berupa keberanian dan kecakapan dalam berbicara, tidak kaku, dan membuat bahasa inggris tampak lebih mudah dipelajari. Celakanya, di dalam pembelajaran dua ragam ini sering kali di vis a vis –kan. Konsekuensinya, mereka yang cenderung pendiam, lebih suka menulis daripada berbicara, sebaliknya meraka yang lebih suka berbicara akan meninggalkan grammar dan lebih memilih berbicara sekenanya walaupun terkesan “hancur” secara gramatika.
+                Dalam buku ini penulisa berusaha untuk menyuguhkan kepada pembaca dengan menawarkan tulisan yang memuat grammar dan speaking secara bersamaan dengan tujuan pembaca dapat menguasai dua ragam bahasa sebagaimana disebutkan di atas dan tidak sampai pada titik kebosanan jika hanya belajar grammar dan speaking secara bersamaan dapat memberikan keunggulan ekstra dari pada belajar hingga tuntas dan akhirnya bosan sebelum dapat terus melanjutkan ke titik yang lainya.
+                Buku ini juga dilengkapi dengan appendix penting, antara lain: Islamic Terms; kamus kecil irregular verbs; proverbs and wise words; other ways to say; british vs American; and animal idioms.
+
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Details Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h3>CV.Idea Sejahtera</h3>
+            <p>
+              Kantor Idea Press <br>
+              Diro RT 58 Pendowoharjo Sewon Bantul<br>
+              Yogyakarta <br><br>
+              <strong>Phone:</strong> +62 818-0407-8229 | +62 857-2727-4416 (Admin)<br>
+              <strong>Email:</strong> ideapress.now@gmail.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Layanan Kami</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Penerbitan Buku</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Setting Layout</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Desain Grafis</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Finishin dan Jilid Binding</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Sosial Media</h4>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Arsha</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
